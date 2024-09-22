36a2dc2eb6a497ad08de7275cd1446be155e6e60
carlhampuswall <carl.hampus.wall@gmail.com>
2024-09-22 15:44:43 +0200

    Added ifndef for max size of log message/origin.

--- src/structs.h
+++ src/structs.h
@@ -1,15 +1,18 @@
 #pragma once
 enum LOG_LEVEL { LOG_LEVEL_INFO = 1, LOG_LEVEL_WARNING, LOG_LEVEL_ERROR, LOG_LEVEL_DEBUG, LOG_LEVEL_COUNT };
 
+#ifndef ORIGIN_MAX_SIZE
 #define ORIGIN_MAX_SIZE 256
+#endif
+
+#ifndef MSG_MAX_SIZE
 #define MSG_MAX_SIZE 1024
+#endif
 
 struct LogMessage {
     LOG_LEVEL level;
     bool verbose;
     unsigned long timestamp;
-    // std::string origin;
-    // std::string msg;
     char origin[ORIGIN_MAX_SIZE];
     char msg[MSG_MAX_SIZE];
 };
